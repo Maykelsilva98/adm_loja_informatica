3e12ec32cd52ad768c1d77eeb2ac7b70efea1dae
Maykelsilva98 <mayckelsantos5@gmail.com>
2022-08-06 18:37:06 -0300

    feat: rotas adicionadas e configuradas para pagina home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home'
 
+function App() {
 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default App
