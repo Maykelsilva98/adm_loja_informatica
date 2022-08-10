fd6f1d3113e212f5cff03bfca59bd2ea56b593b5
Nathali Motooka <nanimotooka@gmail.com>
2022-08-10 00:45:22 -0300

    tentando terminar o post

--- src/Pages/Formulario/Formulario.jsx
+++ src/Pages/Formulario/Formulario.jsx
@@ -1,106 +1,33 @@
-// import React from "react";
-// import style from "./Formulario.module.css";
-// import * as FaIcons from "react-icons/fa";
-
-// const Formulario = () => {
-//   return (
-//     <div>
-//       <form className={style.formulario}>
-//         <div className={style.Nome}>
-//           <label for="nome">Nome</label>
-//           <br></br>
-//           <input
-//             style={{ width: "30%", borderRadius: "" }}
-//             type="text"
-//             id="nome"
-//             name="nome"
-//           ></input>
-//           <br></br>
-//         </div>
-
-//         <div className={style.seis}>
-//           <label for="marca">Marca</label>
-//           <br></br>
-//           <input
-//             style={{ width: "10%", borderRadius: "" }}
-//             type="text"
-//             id="marca"
-//             name="marca"
-//           ></input>
-//           <br></br>
-
-//           <label for="valor">Valor</label>
-//           <br></br>
-//           <input
-//             style={{ width: "10%", borderRadius: "" }}
-//             type="number"
-//             id="valor"
-//             name="valor"
-//           ></input>
-//           <br></br>
-
-//           <label for="ID">Identificação</label>
-//           <br></br>
-//           <input
-//             style={{ width: "10%", borderRadius: "" }}
-//             type="number"
-//             id="ID"
-//             name="ID"
-//           ></input>
-//           <br></br>
-
-//           <label for="cor">Cor</label>
-//           <br></br>
-//           <input
-//             style={{ width: "10%", borderRadius: "" }}
-//             type="text"
-//             id="cor"
-//             name="cor"
-//           ></input>
-//           <br></br>
-
-//           <label for="dimensões">Dimensões</label>
-//           <br></br>
-//           <input
-//             style={{ width: "10%", borderRadius: "" }}
-//             type="text"
-//             id="dimensões"
-//             name="dimensões"
-//           ></input>
-//           <br></br>
-
-//           <label for="peso">Peso</label>
-//           <br></br>
-//           <input
-//             style={{ width: "10%", borderRadius: "" }}
-//             type="text"
-//             id="peso"
-//             name="peso"
-//           ></input>
-//           <br></br>
-//         </div>
-
-//         <label for="descrição">Descrição</label>
-//         <br></br>
-//         <input type="text" id="descrição" name="descrição"></input>
-//         <br></br>
-
-//         <input type="submit" value="ENVIAR" />
-//         <input type="reset" value="CANCELAR" />
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Formulario;
-
-
 import { useState, onChange } from "react";
 import styles from "./Formulario.module.scss";
 // import * as FaIcons from "react-icons/fa";
+import { postProduto } from "../../service/api";
 
 const Formulario = () => {
-  const [imageUrl, setImageUrl] = useState("");
+  const [imageUrl, setImageUrl] = useState();
+  const [dadosForm, setDadosForm] = useState({
+    id: "",
+    nome: "Nathi",
+    cor: "",
+    marca: "",
+    peso: "",
+    tamanho: "",
+    valor: "",
+    descrição: "",
+    imagem_url: "",
+
+  });
+
+  function handleChange(target, key) {
+    const value = target.value;
+    setDadosForm({ ...dadosForm, [key]: value });
+    console.log (dadosForm)
+  }
+
+  async function handleSave() {
+    const msg = await postProduto(dadosForm);
+    setImageUrl(msg)
+  }
 
   return (
     <div>
@@ -112,74 +39,90 @@ const Formulario = () => {
             type="text"
             id="nome"
             name="nome"
-            // onChange={(e) => setImageUrl(e.target.value)}
+            value={dadosForm.nome}
+            onChange={({ target }) => {
+              handleChange(target, "nome");
+            }}
           ></input>
         </div>
 
         <div className={styles.wrapper}>
           <label for="marca">Marca</label>
 
-          <input type="text" id="marca" name="marca"></input>
+          <input type="text" id="marca" name="marca" value={dadosForm.marca}
+            onChange={({target})=>{handleChange(target, 'marca')}}></input>
         </div>
 
         <div className={styles.wrapper}>
           <label for="valor">Valor</label>
 
-          <input type="number" id="valor" name="valor"></input>
+          <input type="number" id="valor" name="valor" value={dadosForm.valor}
+            onChange={({target})=>{handleChange(target, 'valor')}}></input>
         </div>
 
         <div className={styles.wrapper}>
           <label for="ID">Identificação</label>
 
-          <input type="number" id="ID" name="ID"></input>
+          <input type="number" id="ID" name="ID" value={dadosForm.ID}
+            onChange={({target})=>{handleChange(target, 'ID')}}></input>
         </div>
 
         <div className={styles.wrapper}>
           <label for="cor">Cor</label>
 
-          <input type="text" id="cor" name="cor"></input>
+          <input type="text" id="cor" name="cor" value={dadosForm.cor}
+            onChange={({target})=>{handleChange(target, 'cor')}}></input>
         </div>
 
         <div className={styles.wrapper}>
           <label for="dimensões">Dimensões</label>
 
-          <input type="text" id="dimensões" name="dimensões"></input>
+          <input type="text" id="dimensões" name="dimensões" value={dadosForm.tamanho}
+            onChange={({target})=>{handleChange(target, 'tamanho')}}></input>
         </div>
 
         <div className={styles.wrapper}>
           <label for="peso">Peso</label>
 
-          <input type="text" id="peso" name="peso"></input>
+          <input type="text" id="peso" name="peso" value={dadosForm.peso}
+            onChange={({target})=>{handleChange(target, 'peso')}}></input>
         </div>
 
         <div className={`${styles.wrapper} ${styles.wrapper__two}`}>
           <label for="descrição">Descrição</label>
-          <textarea type="text" id="descrição" name="descrição"></textarea>
+          <textarea type="text" id="descrição" name="descrição" value={dadosForm.descricao}
+            onChange={({target})=>{handleChange(target, 'descricao')}}></textarea>
         </div>
 
+        <img
+          src={dadosForm.imagem_url}
+          alt="error"
+          className={styles.imagestest}
+          width={"200vh"}
+          height={"200vh"}
+          margin={"71vh"}
 
-        <img src="" alt="" width={"200vh"} height={"200vh"}/>
-
+        />
 
         <div className={`${styles.wrapper} ${styles.wrapper__two}`}>
           <label for="nome">URL</label>
 
           <input
             type="text"
-            id="nome"
-            name="nome"
-            onChange={(e) => setImageUrl(e.target.value)}
+            id="url"
+            name="url"
+            // onChange={(e) => setImageUrl(e.target.value)}
+            value={dadosForm.imagem_url}
+            onChange={({target})=>{handleChange(target, 'imagem_url')}}
           ></input>
         </div>
 
-
-
-
-        <input type="submit" value="ENVIAR" />
+        <h1 onClick={handleSave()}>teste{imageUrl}</h1>
+        <button onClick={()=>{handleSave()}}>{imageUrl}</button>
         <input type="reset" value="CANCELAR" />
       </form>
 
-      <img src={imageUrl} alt="Error" className={styles.imagestest} />
+      {/* <img src={imageUrl} alt="Error" className={styles.imagestest} /> */}
     </div>
   );
 };
